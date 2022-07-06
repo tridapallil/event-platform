8ca88245496ccf61ef21d98f6124a90949c3226b
Luiz <tridapallil@gmail.com>
2022-07-06 20:51:14 -0300

    Second day developing-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      
-    }
-  }
-`;
-
+import { Event } from "./pages/Event"
 
 function App() {
-  const {data} = useQuery(GET_LESSONS_QUERY);
-
-  console.log(data);
   return (
-    <h1>Tests</h1>
+    <Event />
   )
 }
 
