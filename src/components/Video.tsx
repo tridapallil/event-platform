1ee7a0ff5df1d6d3482bfd48f587690a9760a968
Luiz <tridapallil@gmail.com>
2022-10-09 09:07:57 -0300

    Added Codegen

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,49 +1,21 @@
 import { DefaultUi, Player, Youtube } from '@vime/react';
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
-import { gql, useQuery } from '@apollo/client';
 
 import '@vime/core/themes/default.css';
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String){
-    lesson(where: {slug: $slug}) {
-      videoId
-      title
-      description
-      teacher{
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`;
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    videoId: string;
-    title: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    }
-  }
-}
+import { useGetLessonBySlugQuery } from '../graphql/generated';
 
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-  const {data} = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const {data} = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug
     }
   });
 
-  if (!data){
+  if (!data || !data.lesson){
     return (<div className='flex-1'> <p>Carregando</p></div>);
   }
 
@@ -62,18 +34,19 @@ export function Video(props: VideoProps) {
           <div className="flex-1">
             <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">{data.lesson.description}</p>
-
-            <div className="flex items-center gap-4 mt-6">
-              <img 
-              className="h-16 w-16 rounded-full border-2 border-blue-500"
-              src={data.lesson.teacher.avatarURL}
-              alt=""
-              />
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img 
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                src={data.lesson.teacher.avatarURL}
+                alt=""
+                />
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                  <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,24 +1,22 @@
-import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
-
-const CREATE_SUBSCRIBER_MUTATION = gql`
-mutation CreateSubscriber ($name: String!, $email: String!){
-  createSubscriber(data: {name: $name, email: $email}){
-    id
-  }
-}`;
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
+  const navigate = useNavigate();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const [createSubscriber] = useMutation(CREATE_SUBSCRIBER_MUTATION);
+  const [createSubscriber, {loading}] = useCreateSubscriberMutation();
 
-  const handleSubmitForm = (event: FormEvent) => {
+  const handleSubmitForm = async(event: FormEvent) => {
     event?.preventDefault();
  
-    createSubscriber({variables: { name, email } });
+    await createSubscriber({variables: { name, email } });
+
+    navigate('/event');
   }
   return(
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
@@ -51,7 +49,11 @@ export function Subscribe() {
               placeholder="Digite seu e-mail"
             />
 
-            <button type="submit" className="mt-5 bg-green-500 uppercasep py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors">
+            <button 
+              type="submit" 
+              disabled={loading} 
+              className="mt-5 bg-green-500 uppercasep py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+            >
               Garantir minha vaga
             </button>
           </form>
